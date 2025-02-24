f48c4cb31131d63e8f624650680f7de5c2c59d27
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-24 15:43:53 +0100

    feat: create variables and request input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+let priceTicket;
+const priceForKm = 0.21;
+const discountMinors = 0.20;
+const discountSenior = 0.40;
 
+// Chiedere chilometri da percorre al passeggero
+
+const kilometers = prompt('Quante il numero di chilometri che deve percorre ?');
+
+const age = prompt('Qual è la usa età ?');
