abfb9fbbcad58bf87ebf73f54bbf3d34c93daeb3
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-24 15:45:29 +0100

    feat: add priceTicket and transform string to number

--- js/script.js
+++ js/script.js
@@ -14,3 +14,8 @@ const age = prompt('Qual è la usa età ?');
 if (isNaN(kilometers) || isNaN(age)) {
     alert('ERORRE! Inserire valore valido');
 }
+
+//Trasformare input da stringa a numero
+// Applicare prezzo del biglietto in base ai chilometri (0.21 € al km)
+
+priceTicket = parseInt(kilometers) * priceForKm;
