b62a8ecaf98bb787f04d8277019429f529f833f2
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-24 15:50:37 +0100

    fix: check input

--- js/script.js
+++ js/script.js
@@ -13,24 +13,26 @@ const age = prompt('Qual è la usa età ?');
 
 if (isNaN(kilometers) || isNaN(age)) {
     alert('ERORRE! Inserire valore valido');
-}
-
-//Trasformare input da stringa a numero
-// Applicare prezzo del biglietto in base ai chilometri (0.21 € al km)
-
-priceTicket = parseInt(kilometers) * priceForKm;
-
-// Controllo età
-
-if (age < 18) {
-    // Sconto per minorenni
-    priceTicket = priceTicket * (1 - discountMinors);
-    alert(`Il prezzo del biglietto è ${priceTicket.toFixed(2)} € applicando lo sconto per i minorenni`);
-} else if (age > 65) {
-    // Sconto per over 65
-    priceTicket = priceTicket * (1 - discountSenior);
-    alert(`Il prezzo del biglietto è ${priceTicket.toFixed(2)} € applicando lo sconto per gli over 65`);
 } else {
-    // Nessuno sconto
-    alert(`Il prezzo del biglietto è ${priceTicket.toFixed(2)} €`);
+    //Trasformare input da stringa a numero
+    // Applicare prezzo del biglietto in base ai chilometri (0.21 € al km)
+
+    priceTicket = parseInt(kilometers) * priceForKm;
+
+    // Controllo età
+
+    if (age < 18) {
+        // Sconto per minorenni
+        priceTicket = priceTicket * (1 - discountMinors);
+        alert(`Il prezzo del biglietto è ${priceTicket.toFixed(2)} € applicando lo sconto per i minorenni`);
+    } else if (age > 65) {
+        // Sconto per over 65
+        priceTicket = priceTicket * (1 - discountSenior);
+        alert(`Il prezzo del biglietto è ${priceTicket.toFixed(2)} € applicando lo sconto per gli over 65`);
+    } else {
+        // Nessuno sconto
+        alert(`Il prezzo del biglietto è ${priceTicket.toFixed(2)} €`);
+    }
 }
+
+
